a210d186c91557b33ec1e4f9141638d3a4162245
victor048 <victor.lacerda.m3@gmaail.com>
2024-10-09 22:05:39 -0300

    teste

--- TodoListAPI/Program.cs
+++ TodoListAPI/Program.cs
@@ -50,11 +50,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoListAPI V1");
-        c.RoutePrefix = "swagger";
     });
 }
 
